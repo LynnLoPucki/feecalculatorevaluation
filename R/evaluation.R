2a5e7a98d7bd55ed607ea2b8a2ec83ff675a24a9
David Schnell-Davis <dschnelldavs@gmail.com>
2020-08-22 13:41:02 -0700

    Initial commit

---
+++ R/evaluation.R
@@ -0,0 +1,19 @@
+library(survey)
+#' Evaluation
+#'
+#' Bankruptcy fees evaluation function
+#'
+#' @export
+#' @param input. Required.
+evaluation <- function(inputformula, inputvalues) {
+	useformula <- as.formula(inputformula)
+	usevalues <- as.data.frame(inputvalues)
+	design31 <- svydesign(id = ~casenumber, weights = ~pweight, data = data)
+	model31 <- svyglm(lnfeeexpord ~ lnassets + prepack + preneg + lnsales + xyearfiled + shop, design = design31)
+	newdata1 <- with(data, usevalues)
+	pred <- predict(model31, newdata = newdata1, se.fit = T)
+	efit <- pred[1]
+	lfit <- efit - 1.44 * sqrt(attr(pred, "var")) / .97
+	hfit <- efit + 1.44 * sqrt(attr(pred, "var")) / .97
+	list(efit = efit, hfit = hfit, lfit = lfit)
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+feecalculatorevaluation
+==================
+
+The LoPucki-Doherty Professional Fees Calculator: Evaluation
+
+Function for estimating bankruptcy professional fee and expense awards.
+
+[LoPucki-Doherty Bankruptcy Professional Fees Evaluation Calculator](http://lopucki.law.ucla.edu/fee_calculator_evaluation.asp)
